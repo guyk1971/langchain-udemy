4e1940583890f1f1fdeea853b1cd9bb32c006bf8
guyk1971 <guy.koren@gmail.com>
2025-09-14 18:24:04 +0300

    section 3 completed

--- main_strct.py
+++ main_strct.py
@@ -20,27 +20,27 @@ llm = ChatOpenAI(model="gpt-4")
 # llm = ChatOllama(model="gpt-oss:20b") # doesnt work with react agent yet
 
 react_prompt = hub.pull("hwchase17/react")
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
-react_prompt_with_format_instructions = PromptTemplate(
-    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
-    input_variables=["input", "agent_scratchpad", "tool_names"],
-).partial(format_instructions=output_parser.get_format_instructions())
+# output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+# react_prompt_with_format_instructions = PromptTemplate(
+#     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+#     input_variables=["input", "agent_scratchpad", "tool_names"],
+# ).partial(format_instructions=output_parser.get_format_instructions())
 
 
 agent = create_react_agent(
     llm=llm,
     tools=tools,
-    prompt=react_prompt_with_format_instructions,
-    # prompt=react_prompt,
+    # prompt=react_prompt_with_format_instructions,
+    prompt=react_prompt,
 )
 
 
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-extract_output = RunnableLambda(lambda x: x["output"])
-parse_output = RunnableLambda(lambda x: output_parser.parse(x))
-chain = agent_executor | extract_output | parse_output
-# chain = agent_executor
+# extract_output = RunnableLambda(lambda x: x["output"])
+# parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+# chain = agent_executor | extract_output | parse_output
+chain = agent_executor
 
 
 def main():
